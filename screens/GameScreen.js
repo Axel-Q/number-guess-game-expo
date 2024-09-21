f2237f83fd43396b6feea50d550c48026999915c
Axel <qianwangfan@ultraman.local>
2024-09-21 15:21:07 -0700

    fnish the initial construct of game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,43 @@
 import Colors from "../helperFile/colors";
-import React from "react";
-import {Button, View} from "react-native";
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Alert,
+    TouchableOpacity,
+    Image,
+    Keyboard,
+    ScrollView,
+    TouchableWithoutFeedback,
+    KeyboardAvoidingView,
+    Platform
+} from "react-native";
+import LinearGradientBackground from "../components/LinearGradientBackground";
+import React, {useState, useEffect, useRef, useCallback} from "react";
 
-export default function GameScreen ({user, handleRestart, handleNewGame}) {
+const getRandomNum = () => {
+    return Math.floor(Math.random() * 100) + 1;
+};
+
+export default function GameScreen({handleRestart}) {
+    const [gameState, setGameState] = useState({
+        targetNumber: getRandomNum(),
+        userGuess: "",
+        attemptsLeft: 4,
+        timeLeft: 60,
+        isGameOver: false,
+        hasUsedHint: false,
+        hintMessage: "",
+        feedbackMessage: null,
+    });
+    const timerRef = useRef(null);
+
+
+
+
+
+
     return (
         <View style={styles.base}>
             <View style={styles.infoContainer}>
